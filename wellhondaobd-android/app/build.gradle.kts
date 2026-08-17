2652eb360174dec9a6a0332d0774a8be1aa022b2
wellsound24 <wellsound24@gmail.com>
2026-08-17 17:32:33 +0700

    Bump Well Honda OBD Android to v1.1

--- wellhondaobd-android/app/build.gradle.kts
+++ wellhondaobd-android/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.wellsound24.wellhondaobd"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
     }
 
     buildTypes {
